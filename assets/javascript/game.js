29338c1b7eb1e3a6cc4f1b8bd5c45eabe63266ea
jgallaway2011 <jgallaway2011@gmail.com>
2018-06-18 03:41:41 -0500

    Updating HTML, CSS, & JS docuemnts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,54 +10,41 @@ var losses = 0;
 
 var userScore = 0;
 
-var blueCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
-console.log(blueCrystalRandomNumber);
-
-var yellowCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
-console.log(yellowCrystalRandomNumber);
-
-var greenCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
-console.log(greenCrystalRandomNumber);
-
-var redCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
-console.log(redCrystalRandomNumber);
-
 // FUNCTIONS
 //========================================================================================================================
 
 $(document).ready(function(){
     
-        // Inserts random number into the div with id randomNumber in HTML document
-        $("#randomNumber").html(randomNumber);
-        
-        // Inserts user score into the div with id userScore in the HTML document
-        $("#userScore").html(userScore);
-        
-        // Inserts user score into the div with id userScore in the HTML documen
-        $("#blueCrystal").click(function() {
-            userScore = userScore + blueCrystalRandomNumber;
-            $("#userScore").html(userScore);
-            console.log(userScore);
-        });
-        
-        $("#yellowCrystal").click(function() {
-            userScore = userScore + yellowCrystalRandomNumber;
-            $("#userScore").html(userScore);
-            console.log(userScore);
-        });
-        
-        $("#greenCrystal").click(function() {
-            userScore = userScore + greenCrystalRandomNumber;
-            $("#userScore").html(userScore);
-            console.log(userScore);
-        });
-        
-        $("#redCrystal").click(function() {
-            userScore = userScore + redCrystalRandomNumber;
-            $("#userScore").html(userScore);
-            console.log(userScore);
-        });
+    // Inserts random number into the div with id randomNumber in the index.html document
+    $("#randomNumber").html(randomNumber);
 
+    // Inserts wins into the div with id wins in the index.html document
+    $("#wins").html("Wins: " + wins);
+   
+    // Inserts losses into the div with id wins in the index.html document
+    $("#losses").html("Losses: " + losses);      
+        
+    // Inserts user score into the div with id userScore in the index.html document
+    $("#userScore").html(userScore);
+
+    // Creates four buttons for the crystals using a for loop
+    for (var i = 0; i < 4; i++) {
+        var crystalRandomNumber = Math.floor(Math.random() * 12) + 1;
+        var crystalButton = $("<button>");
+            crystalButton.attr({
+                "class": "crystalButton",
+                "id": "crystalButton" + (i + 1),
+                "crystalRandomNumber": crystalRandomNumber
+            });
+        console.log(crystalRandomNumber);
+        $(".crystalButtons").append(crystalButton);
+    }
+
+    $(".crystalButton").on("click", function() {
+        crystalRandomNumber = parseInt($(this).attr("crystalRandomNumber"));
+        userScore = userScore + crystalRandomNumber;
+        console.log(userScore);
+    });
 
  });
 
