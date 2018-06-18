51e377cb158979133f1a3d71ef65f6ca8d0b26c9
jgallaway2011 <jgallaway2011@gmail.com>
2018-06-18 01:04:02 -0500

    Updating HTML, CSS, & JS docuemnts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
-
 // VARIABLES
 //========================================================================================================================
 
+// Generate Random Number 
 var randomNumber = Math.floor(Math.random() * 102) + 19;
 
 var wins = 0;
@@ -12,31 +11,53 @@ var losses = 0;
 var userScore = 0;
 
 var blueCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
+console.log(blueCrystalRandomNumber);
 
 var yellowCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
+console.log(yellowCrystalRandomNumber);
 
 var greenCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
+console.log(greenCrystalRandomNumber);
 
 var redCrystalRandomNumber = Math.floor(Math.random() * 12) + 1;
+console.log(redCrystalRandomNumber);
 
 // FUNCTIONS
 //========================================================================================================================
 
-    $("#blueCrystal").click(function() {
-        userScore = userScore + blueCrystalRandomNumber;
-    });
+$(document).ready(function(){
+    
+        // Inserts random number into the div with id randomNumber in HTML document
+        $("#randomNumber").html(randomNumber);
+        
+        // Inserts user score into the div with id userScore in the HTML document
+        $("#userScore").html(userScore);
+        
+        // Inserts user score into the div with id userScore in the HTML documen
+        $("#blueCrystal").click(function() {
+            userScore = userScore + blueCrystalRandomNumber;
+            $("#userScore").html(userScore);
+            console.log(userScore);
+        });
+        
+        $("#yellowCrystal").click(function() {
+            userScore = userScore + yellowCrystalRandomNumber;
+            $("#userScore").html(userScore);
+            console.log(userScore);
+        });
+        
+        $("#greenCrystal").click(function() {
+            userScore = userScore + greenCrystalRandomNumber;
+            $("#userScore").html(userScore);
+            console.log(userScore);
+        });
+        
+        $("#redCrystal").click(function() {
+            userScore = userScore + redCrystalRandomNumber;
+            $("#userScore").html(userScore);
+            console.log(userScore);
+        });
 
-    $("#yellowCrystal").click(function() {
-        userScore = userScore + yellowCrystalRandomNumber;
-    });
-
-    $("#greenCrystal").click(function() {
-        userScore = userScore + greenCrystalRandomNumber;
-    });
-
-    $("#redCrystal").click(function() {
-        userScore = userScore + redCrystalRandomNumber;
-    });
 
  });
 
